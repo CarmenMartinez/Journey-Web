1e76d6564f6024e4a32dce6fa1254f0aa8d271b6
marsrmx <marsrmx@gmail.com>
2020-04-23 22:31:07 -0500

    Weird stuff from travel log

--- src/app/travellog.service.ts
+++ src/app/travellog.service.ts
@@ -6,20 +6,41 @@ import { HttpResponse, HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class TravellogService {
-  travelLogs: TravelLog [] = [] 
+  public travelLogs: TravelLog [] = [] 
+  //public travel: TravelLog
 
   constructor(private http:HttpClient) {
+    console.log(0)
     this.http.get('https://9vy4d36mji.execute-api.us-east-1.amazonaws.com/Sandbox/travels', {
       observe: 'response'
-    }).subscribe(
-      (res: HttpResponse<String>) => {
-        this.setTravels(res.body['TravelLogs'])
+    })
+      .subscribe(
+      (res: HttpResponse<TravelLog[]>) => {
+        console.log(1)
+        console.log(res.body['TravelLogs'])
+        let tlogs = res.body['TravelLogs']
+        //this.setTravels(res.body['TravelLogs'])const ids = Object.keys(tlogs)
+        const ids = Object.keys(tlogs)
+        for (const id of ids){
+          const logs = tlogs[id]['logs']
+          
+          let lgs : Log[] = []
+          
+          logs.forEach((l : TravelLog) => {
+            let lng = new Location(l['location']['lat'],  l['location']['long'])
+            let lg = new Log(l['deviceID'],l['timestamp'], lng, l['temperature'])
+            lgs.push(lg)
+          });
+
+          let travelLog = new TravelLog(id, lgs)      
+          this.travelLogs.push(travelLog)
+        }  
       },
       err => console.log(err)
     );
    }
 
-  private setTravels(tlogs) {
+  private setTravels(tlogs: HttpResponse<TravelLog[]>) {
     const ids = Object.keys(tlogs)
     
     for (const id of ids){
@@ -27,34 +48,51 @@ export class TravellogService {
       
       let lgs : Log[] = []
       
-      logs.forEach(l => {
-        let lg: Log = {
-          deviceId : l['deviceID'],
-          timestamp : l['timestamp'],
-          location : {
-            lat: l['location']['lat'],
-            long: l['location']['long']
-          },
-          temperature : l['temperature']
-        }
+      logs.forEach((l : TravelLog) => {
+        let lng = new Location(l['location']['lat'],  l['location']['long'])
+        let lg = new Log(l['deviceID'],l['timestamp'], lng, l['temperature'])
         lgs.push(lg)
       });
 
-      let travelLog: TravelLog = {
-        travelId : id,
-        logs : lgs
-      }
-      
+      let travelLog = new TravelLog(id, lgs)      
       this.travelLogs.push(travelLog)
     }  
   }
 
   getHistoryTravel(): TravelLog[] {
+    console.log(3)
     return this.travelLogs
   }
 
-  getTravelById (){
+  getTravelById (id: string): TravelLog {
 
+    let iterator = this.travelLogs.values()
+    //console.log(iterator.next().value)
+
+    console.log(1)
+    console.log(this.travelLogs)
+    console.log(2)
+    console.log(this.travelLogs.length)
+
+    let index : number = this.travelLogs.findIndex((item) => {
+      //console.log('HELLO', item)
+      ///console.log(item.travelId)
+      return item.travelId == id
+    })
+    index = 0
+    //console.log(this.travelLogs.length)
+    // console.log(typeof(this.travelLogs))
+    // console.log(Object.keys(this.travelLogs))
+    for(let i = 0; i < this.travelLogs.length; i++){
+      //console.log('WHY', this.travelLogs[i].travelId)
+      if(this.travelLogs[i].travelId == id){
+        //console.log(this.travelLogs[i].travelId)
+        index = i
+        break
+      }
+    }
+    //console.log(index)
+    return this.travelLogs[index]
   }
 }
 
--- src/app/travels/travellog/travellog.component.ts
+++ src/app/travels/travellog/travellog.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TravellogService } from 'src/app/travellog.service';
+import { TravelLog } from './Travellog';
 
 @Component({
   selector: 'app-travellog',
@@ -7,11 +8,25 @@ import { TravellogService } from 'src/app/travellog.service';
   styleUrls: ['./travellog.component.css']
 })
 export class TravellogComponent implements OnInit {
-
+  travelLogs : TravelLog []
+  
   constructor(private travellogService: TravellogService) { }
 
   ngOnInit(): void {
-    console.log('GET THIS TRAVELS', this.travellogService.getHistoryTravel())
+    console.log(2)
+    this.travelLogs = this.travellogService.getHistoryTravel()
+    console.log(this.travelLogs)
+    console.log(4)
+    // console.log('GET THIS TRAVELS', t)
+    // console.log('Id = 048b1b73-838c-11ea-9910-c3774c7a5de2', this.travellogService.getTravelById('048b1b73-838c-11ea-9910-c3774c7a5de2'))
+    // let arr : number [] = []
+    // arr.push(4)
+    // console.log(arr)
+  }
+  
+  getTravel(){
+    this.travelLogs = this.travellogService.getHistoryTravel()
+    console.log(this.travelLogs)
   }
 
 }
