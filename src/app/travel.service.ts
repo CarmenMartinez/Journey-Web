00c92d1f951629781eb509585c426220514a88d8
marsrmx <marsrmx@gmail.com>
2020-04-24 00:47:00 -0500

    Travels service fix

--- src/app/travel.service.ts
+++ src/app/travel.service.ts
@@ -7,23 +7,32 @@ import { Travel } from './travels/travel/Travel';
   providedIn: 'root'
 })
 export class TravelService {
+  travels: Travel [] = []
   activeTravel: Travel = new Travel("", "", "", false);
 
   constructor(private http:HttpClient) {
     this.http.get('https://9vy4d36mji.execute-api.us-east-1.amazonaws.com/Sandbox/travel', {
       observe: 'response'
     }).subscribe(
-      (res: HttpResponse<Travel>) => {
-        this.setTravel(res.body['body'])
+      (res: HttpResponse<Travel[]>) => {
+        console.log(res.body)
+        this.travels = res.body;
+        this.setActiveTravel();
+        //this.setTravel(res.body['body'])
       },
       err => console.log(err)
     );
-   }
+  }
+
+  setActiveTravel(){
+    let index = this.travels.findIndex((travel) => travel.status === true)
+    this.activeTravel = this.travels[index]
+  }
 
   private setTravel(response: HttpResponse<Travel>) {
     const id = response["id"];
       let t: Travel = {
-        travelID: id,
+        travelId: id,
         timestamp: "",
         product: "",
         status: true
@@ -32,14 +41,14 @@ export class TravelService {
   }
 
   getActiveTravel(): Travel {
-     return this.activeTravel;
-   }
+    return this.activeTravel;
+  }
 
-   createTravel(product: String) {
-    this.http.post("https://9vy4d36mji.execute-api.us-east-1.amazonaws.com/Sandbox/travel", {
+  createTravel(product: String) {
+  this.http.post("https://9vy4d36mji.execute-api.us-east-1.amazonaws.com/Sandbox/travel", {
       product: product
     }).subscribe(response => {
-       console.log(response);
+      console.log(response);
     });
   }
 
