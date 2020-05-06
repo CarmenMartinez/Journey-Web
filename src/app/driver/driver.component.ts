66569bc7b0b4a564b3bfa52112f65f822a6fa4a7
marsrmx <marsrmx@gmail.com>
2020-05-06 00:16:55 -0500

    Stop travel

--- src/app/driver/driver.component.ts
+++ src/app/driver/driver.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { TravelService } from '../travel.service';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Travel } from '../travels/travel/Travel';
 
 @Component({
   selector: 'app-driver',
@@ -9,12 +10,23 @@ import { Router } from '@angular/router';
   styleUrls: ['./driver.component.css']
 })
 export class DriverComponent implements OnInit {
+  currentTravel: Travel
 
   constructor(private router: Router,
     private travelService: TravelService) { }
 
   ngOnInit(): void {
+    this.getCurrentTravel();
+  }
 
+  getCurrentTravel(){
+    this.currentTravel = this.travelService.getActiveTravel();
+  }
+
+  stopCurrentTravel(){
+    this.travelService.stopTravel(this.currentTravel.travelId)
+    alert("Travel" + this.currentTravel.travelId + " was succesfully stop" )
+    this.getCurrentTravel()
   }
 
   submit(form: NgForm) {
