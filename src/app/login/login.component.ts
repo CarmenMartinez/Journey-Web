2398b4f39664f7dd59ac150d6976994ffeedbf98
Carmen Martinez <carmen.mtzn@gmail.com>
2020-05-09 18:37:06 -0500

    Cognito

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -13,6 +13,7 @@ export class LoginComponent implements OnInit {
     private route: ActivatedRoute,) { }
 
   ngOnInit(): void {
+    window.location.assign("https://journey-web.auth.us-east-1.amazoncognito.com/login?client_id=5m6lhtolg0q7ti41cehk5ccnt5&response_type=code&scope=aws.cognito.signin.user.admin+email+openid+phone+profile&redirect_uri=http://localhost:4200/viajes");
   }
 
   submit(formulario: NgForm) {
